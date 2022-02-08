1c8bbbef0ad7d3a663dc3cf11ff44aaa9fbc71c0
RobertBudiJr <72144055+RobertBudiJr@users.noreply.github.com>
2022-02-08 19:52:18 +0700

    Delete UKL-SPP1 directory

--- UKL-SPP1/admin/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn=mysqli_connect('localhost','root','','bayar_spp');
-/* check connection */
-if (mysqli_connect_error()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-?>
